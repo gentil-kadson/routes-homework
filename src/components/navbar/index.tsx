f72cd448d18b223ffce541d29e0511d11248d3be
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-05-16 20:03:50 -0300

    renomeia o nome da loja.

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <header className="text-2xl text-white flex justify-between items-center shadow-md bg-stone-900 border-1 border-black py-8 px-6">
-      <h1 className="font-bold">Tigrinho Bets</h1>
+      <h1 className="font-bold">Loja do Avô do Yugi</h1>
       <nav className="font-semibold flex items-center text-[1.2rem]">
         <Link href="/about">Sobre</Link>
       </nav>
